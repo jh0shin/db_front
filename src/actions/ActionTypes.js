22e9bf857a0896ff0cdd314ff2052580fcb96c72
Ryu Hwajung <46990156+hystarbit@users.noreply.github.com>
2020-12-01 00:08:58 +0900

    modifying

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,14 @@
+/*
+    Defining Action Types for React Reducer
+*/
+
+// ===== AUTHENTICATION =====
+// Register
+export const AUTH_REGISTER = "AUTH_REGISTER";
+export const AUTH_REGISTER_SUCCESS = "AUTH_REGISTER_SUCCESS";
+export const AUTH_REGISTER_FAILURE = "AUTH_REGISTER_FAILURE";
+
+// TASK
+export const TASK_CREATE = "TASK_CREATE";
+export const TASK_CREATE_SUCCESS = "TASK_CREATE_SUCCESS";
+export const TASK_CREATE_FAILURE = "TASK_CREATE_FAILURE";
---
+++ src/actions/authentication.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import {
+    AUTH_REGISTER,
+    AUTH_REGISTER_SUCCESS,
+    AUTH_REGISTER_FAILURE
+} from './ActionTypes';
+
+// Register
+export function registerRequest(id, password, name, address, gender, phone, birth, role) {
+    return (dispatch) => {
+        // Register API starting
+        dispatch(register());
+
+        return axios.post(
+            'http://localhost:3000/api/register/', {
+                'userid': id,
+                'password': password,
+                'name': name,
+                'address': address,
+                'gender': gender,
+                'phone': phone,
+                'birth': birth,
+                'role': role
+        }).then((response) => {
+            if (response.data['state'] === 'success')
+                dispatch(registerSuccess());
+            else
+                dispatch(registerFailure(response.data['code']))
+        }).catch((error) => {
+            console.log(error);
+            dispatch(registerFailure(error.response.data.code));
+        });
+    };
+}
+
+export function register() {
+    return {
+        type: AUTH_REGISTER
+    };
+}
+
+export function registerSuccess() {
+    return {
+        type: AUTH_REGISTER_SUCCESS,
+    };
+}
+
+export function registerFailure(error) {
+    return {
+        type: AUTH_REGISTER_FAILURE,
+        error
+    };
+}
